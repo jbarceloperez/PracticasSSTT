aa2643cf98a6149d81aff24c8d7e6a54db894b1c
Javier Barceló Pérez <javibarpe@hotmail.com>
2022-06-14 01:52:48 +0200

    testing

--- CodigoServidor/web_sstt.py
+++ CodigoServidor/web_sstt.py
@@ -212,6 +212,7 @@ def process_web_request(cs, webroot):
                     # Si la cabecera es Cookie comprobar  el valor de cookie_counter para ver si 
                     # ha llegado a MAX_ACCESOS y devolver un Error "403 Forbidden"
                     if heads!=-1:
+                        logger.debug("Cabeceras:\n" + heads)
                         if "Cookie" in heads:
                             cookie_counter = process_cookies(heads["Cookie"], s)
                             if cookie_counter!=-1:  # si no se ha mandado un mensaje forbidden
